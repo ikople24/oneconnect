d1250abc8b596baf03fc6ef271dbd3f4110e4d5e
KOPLE <45056298+ikople24@users.noreply.github.com>
2025-01-21 11:13:45 +0700

    shade css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors:{
-        primaryTitle: "#000957",
-        primaryContent: "#344CB7",
-        primarySubcontent: "#FFEB00",
-        primaryBase: "#fdfdfd",
-        primaryAccent: "#577BC1",
-        primaryBg: "#f6f6f6",
-      }
-    },
+  	extend: {
+  		colors: {
+  			primaryTitle: '#000957',
+  			primaryContent: '#344CB7',
+  			primarySubcontent: '#FFEB00',
+  			primaryBase: '#fdfdfd',
+  			primaryAccent: '#577BC1',
+  			primaryBg: '#f6f6f6'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
