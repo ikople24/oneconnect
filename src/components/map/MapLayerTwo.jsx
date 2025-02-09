a7049b9fae1fa05100a43b2e62dc931de62b64a8
anuchacodes <anucha.codes@gmail.com>
2025-02-09 22:02:51 +0700

    feat(marker): add data table, modal detail & handle delete method

--- src/components/map/MapLayerTwo.jsx
+++ src/components/map/MapLayerTwo.jsx
@@ -8,7 +8,7 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import React from "react";
-import { Button } from "antd";
+import { Space, Button, Table, Tooltip, Modal, message } from "antd";
 import { GeoJSON, useMapEvent, LayersControl } from "react-leaflet";
 import { ENDPOINT } from "../endpoint";
 import { SwitchMode } from "../admin/SwitchMode";
@@ -25,6 +25,17 @@ export default function MapLayerTwo(props) {
   const [pinTypes, setPinTypes] = useState([]);
   const [currentMarker, setCurrentMarker] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  // TODO:: สำหรับตาราง admin
+  const [tableParams, setTableParams] = useState({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+      total: 0,
+    },
+  });
+  const [modalMarkerIsvisible, setModalMarkerIsvisible] = useState(false);
+  const [selectedRecord, setSelectedRecord] = useState(null);
+
   useEffect(() => {
     // console.log(place);
     const fetchData = async () => {
@@ -221,11 +232,108 @@ export default function MapLayerTwo(props) {
       } else {
         await Promise.allSettled([fetchMarkers(place?._id)]);
       }
-      setIsModalVisible(!isModalVisible)
+      setIsModalVisible(!isModalVisible);
     } catch (error) {
       console.log("error", error);
     }
   };
+
+  // TODO:: สำรหับตาราง Table
+  const handleView = (record) => {
+    console.log("ดูรายละเอียด", record);
+    setSelectedRecord(record);
+    setModalMarkerIsvisible(!modalMarkerIsvisible);
+  };
+
+  const deleteMarker = async (id) => {
+    return await fetch(`${ENDPOINT.DELETE_MARKER_ADMIN}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
+  const handleDelete = (record) => {
+    console.log("ลบ", record);
+    Modal.confirm({
+      title: "คุณแน่ใจไหม?",
+      content: "หากคุณลบข้อมูลนี้จะไม่สามารถกู้คืนได้",
+      okText: "ใช่",
+      cancelText: "ยกเลิก",
+      async onOk() {
+        const { _id } = record;
+        try {
+          await deleteMarker(_id);
+
+          message.success({
+            content: "ลบข้อมูลสำเร็จ!",
+            duration: 3,
+          });
+        } catch (error) {
+          message.error({
+            content: error.message || "เกิดข้อผิดพลาดในการลบข้อมูล",
+            duration: 3,
+          });
+        }
+      },
+      onCancel() {
+        console.log("ยกเลิกการลบ");
+      },
+    });
+  };
+
+  const columns = [
+    {
+      title: "ลำดับ",
+      dataIndex: "index",
+      key: "index",
+      align: "center",
+      width: 100,
+      render: (text, record, index) => index + 1,
+    },
+    {
+      title: "ชื่อข้อมูล",
+      dataIndex: ["properties", "name"],
+      key: "name",
+      align: "center",
+      width: 200,
+    },
+    {
+      title: "ประเภทข้อมูล",
+      dataIndex: ["properties", "markerType"],
+      key: "markerType",
+      align: "center",
+      width: 200,
+    },
+    {
+      title: "แอคชั่น",
+      key: "action",
+      align: "center",
+      width: 200,
+      render: (text, record) => (
+        <Space>
+          <Tooltip title="ดูรายละเอียด" open={false}>
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<EyeOutlined />}
+              onClick={() => handleView(record)}
+            />
+          </Tooltip>
+          <Tooltip title="ลบ" open={false}>
+            <Button
+              color="danger"
+              variant="solid"
+              shape="circle"
+              icon={<DeleteOutlined />}
+              onClick={() => handleDelete(record)}
+            />
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ];
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
@@ -338,7 +446,22 @@ export default function MapLayerTwo(props) {
           </ul>
         </div>
       </div>
-
+      {/*  ตาราง แสดงข้อมูล Admin  */}
+      <div>
+        <Table
+          columns={columns}
+          dataSource={markers}
+          rowKey="_id"
+          pagination={tableParams.pagination}
+          scroll={{ x: "max-content" }}
+        />
+      </div>
+      {/* Modal Marker Detail in Table */}
+      <ModalMarkerDetail
+        visible={modalMarkerIsvisible}
+        onCancel={() => setModalMarkerIsvisible(false)}
+        data={selectedRecord}
+      />
       {/* Modal */}
       <ModalAddMarker
         visible={isModalVisible}
