aefcd57064c3074f940c9fefd53b564d9c3e7c74
liangkhwai <liangkhwai@gmail.com>
2025-03-01 01:17:43 +0700

    feat(context): init context provider

--- src/components/map/MapLayerTwo.jsx
+++ src/components/map/MapLayerTwo.jsx
@@ -27,11 +27,10 @@ import { useUser } from "@clerk/clerk-react";
 import Role from "@/enum/role.enum";
 export default function MapLayerTwo(props) {
   const { place, changePage } = props;
-  const { TEST, setTest } = useGlobalContext();
+  const { checkIsAdminPlace } = useGlobalContext();
   const markerRef = useRef(null);
 
-  const user = useUser();
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(checkIsAdminPlace(place?._id));
   const [pointSelected, setPointSelected] = useState(
     isAdmin && place?.location?.coordinates
   );
@@ -47,25 +46,7 @@ export default function MapLayerTwo(props) {
   const [isLatLngError, setIsLatLngError] = useState(false);
   const [isTriggerReq, setIsTriggerReq] = useState(false);
 
-  console.log("user info from clerk", user);
   useEffect(() => {
-    if (user?.user?.publicMetadata?.role) {
-      const userRole = user.user.publicMetadata.role;
-      const userPlaceId = user.user.publicMetadata.placeId;
-
-      if (userRole === Role.SUPER_ADMIN) {
-        setIsAdmin(true);
-      } else if (userRole === Role.ADMIN && place?._id === userPlaceId) {
-        setIsAdmin(true);
-      } else {
-        setIsAdmin(false);
-      }
-    } else {
-      setIsAdmin(false);
-    }
-  }, [user, place]);
-  useEffect(() => {
-    console.log("FROM CONTEXT", TEST);
     fetchData();
   }, [isAdmin]);
   const fetchData = async () => {
